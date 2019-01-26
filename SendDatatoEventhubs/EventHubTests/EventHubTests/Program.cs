fc77f4933a31d43456dfc884b30d25f2bf5500bf
Sudhie Siddautam <sudies@microsoft.com>
2019-01-25 22:23:25 -0800

    Send data to event hubs in Azure

---
+++ SendDatatoEventhubs/EventHubTests/EventHubTests/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.EventHubs;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubTests
+{
+    class Program
+    {
+        private static EventHubClient eventHubClient;
+
+        private static async Task MainAsync(string[] args)
+        {
+            string connectionString = Secretlookup.GetKey();
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString);
+
+            await SendMessagesToEventHub(10);
+
+            await eventHubClient.CloseAsync();
+
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+
+        private static async Task SendMessagesToEventHub(int numMessagesToSend)
+        {
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    var message = $"Message {i}";
+                    Console.WriteLine($"Sending message: {message}");
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                await Task.Delay(10);
+            }
+
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
+        }
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ SendDatatoEventhubs/EventHubTests/EventHubTests/Secretlookup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EventHubTests
+{
+    class Secretlookup
+    {
+        public static string GetKey()
+        {
+            string key = null;
+            string filePath = @"c:\temp\testeventhub.txt";
+            if (File.Exists(filePath))
+            {
+                key = File.ReadAllText(filePath);
+            }
+            return key;
+        }
+    }
+}
